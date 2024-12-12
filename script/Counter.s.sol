6f5df3e9c0dcebed81aa990231b62de5c52daf38
muhammadraazy <muhammadrazi51@gmail.com>
2024-12-12 07:37:17 -0800

    implement a trade

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -6,6 +6,8 @@ import {Script, console} from "forge-std/Script.sol";
 contract CounterScript is Script {
     function setUp() public {}
 
+    // 0x694AA1769357215DE4FAC081bf1f309aDC325306 price feeds sepolia
+
     function run() public {
         vm.broadcast();
     }
--- src/Trade.sol
+++ src/Trade.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import { ISwapRouter } from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { AggregatorV3Interface } from "@chainlink/contracts/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import { TransferHelper } from "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 
 contract Trade is Ownable {
 
@@ -12,9 +13,11 @@ contract Trade is Ownable {
     
     address private s_owner; // initial owner
     bool private s_tradePaused = false;
-    uint256 private s_tradeFeePercentage = 2; // 0.02%
-    // 0x694AA1769357215DE4FAC081bf1f309aDC325306 price feeds sepolia
+    uint256 private s_poolFee = 3000; // 0.3%
+    uint256 private s_protocolFee = 2000; // 0.2%
+    
     AggregatorV3Interface private s_priceFeed;
+    ISwapRouter private immutable i_swapRouter; // uniswap router v3
 
     // ---------------------------------------- EVENTS ---------------------------------------
     event TradePaused(address indexed admin);
@@ -22,6 +25,7 @@ contract Trade is Ownable {
     constructor(address router_, address owner_, address priceFeed_) Ownable(owner_) {
         s_owner = owner_;
         s_priceFeed = AggregatorV3Interface(priceFeed_);
+        i_swapRouter = ISwapRouter(router_);
     }
 
 
@@ -31,6 +35,9 @@ contract Trade is Ownable {
         _;
     }
 
+    /**
+        @dev only owner that can pause the trade
+     */
     function pauseTrade() external onlyOwner InvalidCaller {
         bool isTradePaused = s_tradePaused;
         if(isTradePaused == false) revert Trade__TradeIsPaused();
@@ -38,11 +45,34 @@ contract Trade is Ownable {
         emit TradePaused(owner());
     }
 
+
     function tradePayNative(uint256 amount, address token, uint256 minOutAmount) external payable InvalidCaller {
 
     }
 
-    function tradePayToken() external InvalidCaller {}
+    function tradePayToken(address tokenIn, address tokenOut, uint256 amountIn) external InvalidCaller {
+        uint256 poolFee = s_poolFee;
+
+        // transfer token into this contract, caller must approve this txs
+        TransferHelper.safeTransferFrom(tokenIn, msg.sender, address(this), amountIn);
+        // approving swap_router to pull the token from this contract
+        TransferHelper.safeApprove(tokenIn, address(i_swapRouter), amountIn);
+
+        ISwapRouter.ExactInputSingleParams memory params =
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: tokenIn,
+                tokenOut: tokenOut,
+                fee: poolFee,
+                recipient: _msgSender(),
+                deadline: block.timestamp,
+                amountIn: amountIn,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+
+        // The call to `exactInputSingle` executes the swap.
+        amountOut = i_swapRouter.exactInputSingle(params);
+    }
 
     receive() external payable {}
 }
