2d2139c5be3190f84754b13fef564813c1a02bc1
raazymuhd1 <muhammadrazi51@gmail.com>
2025-03-06 18:25:38 +0700

    added a helperConfig on DeployTrade contract

--- script/DeployTrade.sol
+++ script/DeployTrade.sol
@@ -2,14 +2,20 @@
 pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
+import { HelperConfig } from "./HelperConfig.s.sol";
 import { YoloTrade } from "../src/YoloTrade.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
+    HelperConfig helperConfig;
 
     // 0x694AA1769357215DE4FAC081bf1f309aDC325306 price feeds sepolia
 
     function run() public {
         vm.broadcast();
+        helperConfig = new HelperConfig();
+        (address quoter, , , , , , ) = helperConfig.s_networkConfig();
+
+        console.log(quoter);
     }
 }
